5a0be3b2ec3f8b8071449fbe8db7b2e03d30406a
Trinh Duc Thinh <trinhthinh388@gmail.com>
2018-05-24 11:06:27 +0700

    New Change

---
+++ Sample PJ/Assets/Scripts/Managers/CoinManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+namespace CompleteProject
+{
+    public class CoinManager : MonoBehaviour
+    {
+
+        public Text coinText;
+        int startCoin = 0;
+        public static int currentCoin;
+        // Use this for initialization
+        void Start()
+        {
+            currentCoin = startCoin;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            coinText.text = currentCoin.ToString();
+        }
+    }
+}
---
+++ Sample PJ/Assets/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+namespace CompleteProject
+{
+    public class ScoreManager : MonoBehaviour
+    {
+        public static float score;        // The player's score.
+        public PlayerMovement playerMovement;
+        public PlayerHealth playerHealth;
+
+        public Text text;                      // Reference to the Text component.
+
+
+        void Awake ()
+        {
+            score = 0;
+        }
+
+        void Update ()
+        {
+
+            if (!playerMovement.walking || playerHealth.isDead)
+            {
+                return;
+            }
+
+            score += playerMovement.GetMovement().magnitude;
+            // Set the displayed text to be the word "Score" followed by the score value.
+            text.text = score.ToString() + " m";
+        }
+    }
+}
